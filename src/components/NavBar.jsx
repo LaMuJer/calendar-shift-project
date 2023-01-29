cd0e3ad4e459696d1a585e2d56aaeff4924e5a14
laMuJer <lareina1467.com>
2023-01-30 02:48:18 +0630

    tables

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,27 @@
+import { Stack } from "@mui/system"
+import { Link } from "react-router-dom"
+import Typography from '@mui/material/Typography'
+
 const NavBar = () => {
+
     return (
-        <div>NavBar</div>
+        <Stack direction={'row'} p='2' alignItems={'center'}
+            sx={{
+                position: 'sticky',
+                bgcolor: '#fff',
+                top: '0',
+                justifyContent: 'center',
+            }}
+        >
+            <Link to={'/'} style={{ textDecoration: 'none' }} >
+                <Typography variant="body2" className='link' color="initial">Home</Typography>
+            </Link>
+            <Link to={'/shift/:id'} style={{ textDecoration: 'none', margin: '0px 20px' }}   >
+                <Typography variant="body2" className='link' color="initial">Shift</Typography>
+            </Link>
+            <Typography variant="body2" className='link' color="initial">About</Typography>
+
+        </Stack>    
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,47 @@
+import { Box } from '@mui/material'
+
 const Home = () => {
     return (
-        <div>Home</div>
+        <Box>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Day</th>
+                        <th>Shift
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Shift A</th>
+                                        <th>Shift B</th>
+                                        <th>Shift C</th>
+                                        <th>Shift D</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>A</td>
+                        <td>A</td>
+                        <td>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>Off</td>
+                                        <td>Morning</td>
+                                        <td>Night</td>
+                                        <td>Off</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </Box>
     )
 }
 
