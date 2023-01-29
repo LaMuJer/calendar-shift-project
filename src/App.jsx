024575200f265c8aacae604c621b38fd9b9d547e
laMuJer <lareina1467.com>
2023-01-30 01:47:00 +0630

    routes a

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./components/Home"
+import Home from "./pages/Home"
 import NavBar from "./components/NavBar"
-import Shift from "./components/Shift"
+import Shift from "./pages/Shift"
+import {Box } from "@mui/material"
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Box >
+      <Box>
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
