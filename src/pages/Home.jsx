b68c8b42e2e8bf528b793866687e299e50d1b277
Terry <terry@Terry-Macbook-Pro.local>
2023-01-30 23:51:56 +0630

    add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,63 +1,59 @@
-import { Box } from '@mui/material'
-import { MenuOutlined } from '@mui/icons-material'
-import { useState } from 'react'
+import { Box } from "@mui/material";
+import { MenuOutlined } from "@mui/icons-material";
+import { useState } from "react";
+import data from "../utils/date";
 
 const Home = () => {
+  const [show, setshow] = useState();
 
-    const [show, setshow] = useState()
+  const date = new Date();
 
-    // const handleshow = () {
+  console.log(
+    date.toLocaleDateString(),
+    date.toLocaleDateString("us", { weekday: "long" })
+  );
 
-    // }
+  // const handleshow = () {
 
-    return (
+  // }
 
-        <Box sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            mt: '30px'
-        }}>            
-            <table className="shiftSheet">
-                <thead>
-                    <tr >
-                        <th>Date</th>
-                        <th>Day</th>
-                        <th>Shift A</th>
-                        <th>Shift B</th>
-                        <th>Shift C</th>
-                        <th>Shift D</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>30/1/23</td>
-                        <td>Monday</td>
-                        <td>Off</td>
-                        <td>Off</td>
-                        <td>Morning</td>
-                        <td>Morning</td>
-                    </tr>
-                    <tr className='highlight'>
-                        <td>1/2/23</td>
-                        <td>Tuesday</td>
-                        <td>Night</td>
-                        <td>Night</td>
-                        <td>Off</td>
-                        <td>Off</td>
-                    </tr>
-                    <tr>
-                        <td>2/2/23</td>
-                        <td>Wednesday</td>
-                        <td>Off</td>
-                        <td>Off</td>
-                        <td>Morning</td>
-                        <td>Morning</td>
-                    </tr>
-                </tbody>
-            </table >
-        </Box>
-    )
-}
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        mt: "30px",
+      }}
+    >
+      <table className="shiftSheet">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Day</th>
+            <th>Shift A</th>
+            <th>Shift B</th>
+            <th>Shift C</th>
+            <th>Shift D</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((i, index) => (
+            <tr className={index % 2 == 0 && "hightlight"}>
+              <td>{new Date(i.date).toLocaleDateString()}</td>
+              <td>
+                {new Date(i.date).toLocaleDateString("us", { weekday: "long" })}
+              </td>
+              <td>{i.assign.A}</td>
+              <td>{i.assign.B}</td>
+              <td>{i.assign.C}</td>
+              <td>{i.assign.D}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
--- src/pages/Shift.jsx
+++ src/pages/Shift.jsx
@@ -1,48 +1,60 @@
-import React, { useState } from "react"
-import '../App.css'
-import { Box, Button, Stack, Typography } from "@mui/material"
-import { lists } from "../utils/constant"
-import CalendarModel from "../components/CalendarModel"
+import React, { useState } from "react";
+import "../App.css";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import { lists } from "../utils/constant";
+import CalendarModel from "../components/CalendarModel";
 
 const Shift = () => {
-
-
-    return (
-
-        <Stack sx={{
-            width: '100vw',
-            height: '100vh',
-            display: 'flex',
-            justifyContent: 'start',
-            alignItems: 'center'
-        }}>
-            <Box sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'start',
-                alignItems: 'center'
-            }} >
-                <Button variant="outlined" color="info" style={{ marginTop: '30px' }} >
-                    <p style={{ fontSize: '10px' }}>Edit</p>
-                </Button>
-                <CalendarModel />
-                <Stack direction={'row'} sx={{
-                    width: '100vw',
-                    height: '20vh',
-                    bgcolor: 'yellow',
-                    mt: '30px'
-                }}>
-                    {lists.map(({ shift, duration, icon }, index) => {
-                        <div key={index}>
-                            <Typography variant="subtitle2" color='success'>{shift}</Typography>
-                            <Typography variant="subtitle2" color='success'>{duration}</Typography>
-                            <Typography variant="subtitle2" color='success'>{React.createElement(icon)}</Typography>
-                        </div>
-                    })}
-                </Stack>
-            </Box>
+  return (
+    <Stack
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "start",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "start",
+          alignItems: "center",
+        }}
+      >
+        <Button variant="outlined" color="info" style={{ marginTop: "30px" }}>
+          <p style={{ fontSize: "10px" }}>Edit</p>
+        </Button>
+        <CalendarModel />
+        <Stack
+          direction={"row"}
+          sx={{
+            width: "100vw",
+            height: "20vh",
+            bgcolor: "yellow",
+            mt: "30px",
+          }}
+        >
+          {lists.map(({ shift, duration, icon }, index) => {
+            return (
+              <div key={index}>
+                <Typography variant="subtitle2" color="success">
+                  {shift}
+                </Typography>
+                <Typography variant="subtitle2" color="success">
+                  {duration}
+                </Typography>
+                <Typography variant="subtitle2" color="success">
+                  {React.createElement(icon)}
+                </Typography>
+              </div>
+            );
+          })}
         </Stack>
-    )
-}
+      </Box>
+    </Stack>
+  );
+};
 
-export default Shift
+export default Shift;
