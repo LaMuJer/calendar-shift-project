048a39938962a21368995bb46eb727ee3066b0d5
Terry <terry@Terry-Macbook-Pro.local>
2023-02-02 23:41:42 +0630

    loadmore

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,34 @@
 import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
 import data from "../utils/date";
 
 const Home = () => {
+  const [loadMore, setLoadMore] = useState({
+    count: 10,
+    con: true,
+  });
 
-  const date = new Date();
+  const date = data.filter((i) => {
+    let tomorrow = new Date(i.date).getTime() + 24 * 60 * 60 * 1000;
+    return new Date(tomorrow).getTime() >= new Date().getTime();
+  });
 
-  console.log(
-    date.toLocaleDateString(),
-    date.toLocaleDateString("en", { weekday: "long" })
-  );
+  date.splice(loadMore.count, data.length - 1);
+
+  const handleLoadMore = () => {
+    setLoadMore((pre) => ({ ...pre, count: pre.count + 10 }));
+  };
+
+  useEffect(() => {
+    console.log();
+
+    if (
+      new Date(date[date.length - 1].date).getTime() ===
+      new Date(data[data.length - 1].date).getTime()
+    ) {
+      setLoadMore((pre) => ({ ...pre, con: false }));
+    }
+  }, [loadMore.count]);
 
   return (
     <Box
@@ -17,10 +37,11 @@ const Home = () => {
         justifyContent: "center",
         alignItems: "center",
         mt: "30px",
+        flexDirection: "column",
       }}
     >
       <table className="shiftSheet ">
-        <thead className="sticky" >
+        <thead className="sticky">
           <tr>
             <th>Date</th>
             <th>Shift A</th>
@@ -30,23 +51,23 @@ const Home = () => {
           </tr>
         </thead>
         <tbody>
-          {data.map((i, index) => (
-            <tr
-              key={index}
-              className={
-                new Date().toLocaleDateString() ===
-                  new Date(i.date).toLocaleDateString() ? "highlight" : ""
-              }
-            >
-              <td style={{ display: 'flex', justifyContent: 'start' }}>
-                <p style={{ width: '100px' }}>
-                  {new Date(i.date).toLocaleDateString("en-GB", {
-                    day: 'numeric',
-                    month: 'short',
-                    year: 'numeric'
-                  }).replace(/ /g , '-')}
+          {date.map((i, index) => (
+            <tr key={index} className={index === 0 ? "highlight" : ""}>
+              <td style={{ display: "flex", justifyContent: "start" }}>
+                <p style={{ width: "100px" }}>
+                  {new Date(i.date)
+                    .toLocaleDateString("en-GB", {
+                      day: "numeric",
+                      month: "short",
+                      year: "numeric",
+                    })
+                    .replace(/ /g, "-")}
                 </p>
-                <p>{new Date(i.date).toLocaleDateString("us", { weekday: "long" })}</p>
+                <p>
+                  {new Date(i.date).toLocaleDateString("us", {
+                    weekday: "long",
+                  })}
+                </p>
               </td>
               <td>{i.assign.A}</td>
               <td>{i.assign.B}</td>
@@ -56,6 +77,8 @@ const Home = () => {
           ))}
         </tbody>
       </table>
+
+      {loadMore.con && <button onClick={handleLoadMore}>Load More</button>}
     </Box>
   );
 };
